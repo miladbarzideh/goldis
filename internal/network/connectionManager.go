35741fb527e76a70fc18ffb70be78ca0affee4a6
Milad Barzideh <milad.barzideh@snapp.cab>
2023-08-08 15:27:40 +0330

    feat(network): a connection handler to manage incoming requests based on event loop

---
+++ internal/network/connectionManager.go
@@ -0,0 +1,132 @@
+package network
+
+import (
+	"fmt"
+	"log"
+	"syscall"
+)
+
+// ConnectionHandler handles the connection management logic
+type ConnectionHandler struct {
+	socket   *Socket
+	fdMax    int
+	activeFD syscall.FdSet
+	fdConn   FdConn
+}
+
+// NewConnectionHandler creates a new instance of ConnectionManager
+func NewConnectionHandler(socket *Socket) *ConnectionHandler {
+	serverFd := socket.Fd
+	var activeFd syscall.FdSet
+	FDZero(&activeFd)
+	FDSet(serverFd, &activeFd)
+	fdConn := FDConnInit()
+	return &ConnectionHandler{
+		socket:   socket,
+		fdMax:    serverFd,
+		activeFD: activeFd,
+		fdConn:   fdConn,
+	}
+}
+
+// StartServer starts the server and handles the connection management logic
+func (cm *ConnectionHandler) StartServer() {
+	for {
+		activeFDSet := cm.getActiveFDSet()
+		err := syscall.Select(cm.fdMax+1, &activeFDSet, nil, nil, nil)
+		if err != nil {
+			log.Fatal("Select(): ", err)
+		}
+		cm.handleActiveConnections(activeFDSet)
+	}
+}
+
+func (cm *ConnectionHandler) addConnection(connection *Connection) {
+	acceptedFd := connection.Fd
+	FDSet(acceptedFd, &cm.activeFD)
+	cm.fdConn.Set(acceptedFd, *connection)
+	if acceptedFd > cm.fdMax {
+		cm.fdMax = acceptedFd
+	}
+}
+
+func (cm *ConnectionHandler) destroyConnection(connection Connection) {
+	FDClr(connection.Fd, &cm.activeFD)
+	cm.fdConn.Clr(connection.Fd)
+	_ = connection.Close()
+}
+
+func (cm *ConnectionHandler) handleConnectionIO(fd int) {
+	connection := cm.fdConn.Get(fd)
+	msg, err := connection.Read()
+	if err != nil {
+		fmt.Println("Read(): ", err)
+		cm.destroyConnection(connection)
+		return
+	}
+
+	_, err = connection.Write([]byte(msg))
+	if err != nil {
+		fmt.Println("Write(): ", err)
+	}
+}
+
+func (cm *ConnectionHandler) getActiveFDSet() syscall.FdSet {
+	tmpFDSet := cm.activeFD
+	return tmpFDSet
+}
+
+func (cm *ConnectionHandler) handleActiveConnections(fdSet syscall.FdSet) {
+	// the event loop
+	for fd := 0; fd < cm.fdMax+1; fd++ {
+		if FDIsSet(fd, &fdSet) {
+			if fd == cm.socket.Fd {
+				cm.acceptNewConnection()
+			} else {
+				cm.handleConnectionIO(fd)
+			}
+		}
+	}
+}
+
+func (cm *ConnectionHandler) acceptNewConnection() {
+	connection, err := cm.socket.Accept()
+	if err != nil {
+		log.Fatal("Accept(): ", err)
+	}
+	cm.addConnection(connection)
+}
+
+func FDIsSet(fd int, p *syscall.FdSet) bool {
+	return p.Bits[fd/32]&(1<<(uint(fd)%32)) != 0
+}
+
+func FDSet(fd int, p *syscall.FdSet) {
+	p.Bits[fd/32] |= 1 << (uint(fd) % 32)
+}
+
+func FDClr(fd int, p *syscall.FdSet) {
+	p.Bits[fd/32] &^= 1 << (uint(fd) % 32)
+}
+
+type FdConn map[int]Connection
+
+func FDConnInit() FdConn {
+	return make(FdConn, syscall.FD_SETSIZE)
+}
+
+func (f *FdConn) Get(fd int) Connection {
+	return (*f)[fd]
+}
+
+func (f *FdConn) Set(fd int, value Connection) {
+	(*f)[fd] = value
+}
+
+func (f *FdConn) Clr(fd int) {
+	delete(*f, fd)
+}
+
+func FDZero(p *syscall.FdSet) {
+	p.Bits = [32]int32{}
+}
