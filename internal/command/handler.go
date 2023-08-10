91ccbd70ef090fbbb34343277d40011a070df607
Milad Barzideh <milad.barzideh@snapp.cab>
2023-08-10 13:55:34 +0330

    refactor(command): case insensitive comparison

--- internal/command/handler.go
+++ internal/command/handler.go
@@ -3,6 +3,7 @@ package command
 import (
 	"log"
 	"regexp"
+	"strings"
 
 	"github.com/miladbarzideh/goldis/internal/repository"
 )
@@ -24,11 +25,11 @@ func (h *Handler) Execute(input string) string {
 	}
 	command, args := commandParts[0], commandParts[1:]
 	log.Printf("Command %s will be executed", command)
-	if command == "set" && len(args) == 2 {
+	if strings.EqualFold(command, "set") && len(args) == 2 {
 		return h.dataSource.Set(args[0], args[1])
-	} else if command == "get" && len(args) == 1 {
+	} else if strings.EqualFold(command, "get") && len(args) == 1 {
 		return h.dataSource.Get(args[0])
-	} else if command == "del" && len(args) == 1 {
+	} else if strings.EqualFold(command, "del") && len(args) == 1 {
 		return h.dataSource.Delete(args[0])
 	}
 	return syntaxErrorMsg
