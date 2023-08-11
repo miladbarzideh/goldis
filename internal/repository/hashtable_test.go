e0c0df4ffa47aa59916d62df991c86b30f451d66
Milad Barzideh <milad.barzideh@snapp.cab>
2023-08-11 20:43:13 +0330

    test(hashtable): add a test to insert into the hashmap

---
+++ internal/repository/hashtable_test.go
@@ -0,0 +1,27 @@
+package repository
+
+import "testing"
+
+func TestNewHMap(t *testing.T) {
+	hmap := NewHMap()
+
+	if hmap.tab1.size != 0 {
+		t.Errorf("Expected tab1 size to be 0, got %d", hmap.tab1.size)
+	}
+	if hmap.tab2.tab != nil {
+		t.Error("Expected tab2 to be nil")
+	}
+}
+
+func TestHMapInsert(t *testing.T) {
+	hmap := NewHMap()
+
+	for i := 0; i < 3; i++ {
+		node := &HNode{hcode: hash("key")}
+		hmap.insert(node)
+		expectedSize := i + 1
+		if hmap.tab1.size != expectedSize {
+			t.Errorf("Expected tab1 size to be %d, got %d", expectedSize, hmap.tab1.size)
+		}
+	}
+}
