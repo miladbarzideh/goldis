a45839ce90a3245e2138293aa7b36f64df2d419e
Milad Barzideh <milad.barzideh@snapp.cab>
2023-08-17 12:04:21 +0330

    refactor(avl): remove the entry.go

--- internal/datastore/avl.go
+++ internal/datastore/avl.go
@@ -167,3 +167,25 @@ func (node *AVLNode) getHeight() uint32 {
 	}
 	return node.height
 }
+
+type AVLEntry struct {
+	node  AVLNode
+	value int32
+}
+
+func NewAVLEntry(value int32) *AVLEntry {
+	return &AVLEntry{
+		value: value,
+	}
+}
+
+func avlEntryEq(l, r *AVLNode) int {
+	le := (*AVLEntry)(containerOf(unsafe.Pointer(l), unsafe.Offsetof(AVLEntry{}.node)))
+	re := (*AVLEntry)(containerOf(unsafe.Pointer(r), unsafe.Offsetof(AVLEntry{}.node)))
+	if le.value > re.value {
+		return 1
+	} else if le.value < re.value {
+		return -1
+	}
+	return 0
+}
