10157ed5c2898865e971df23ae13a677f8cbf7e4
Milad Barzideh <milad.barzideh@snapp.cab>
2023-08-17 21:28:41 +0330

    feat(handler): add sorted set (zset) related command to handler

--- internal/command/handler.go
+++ internal/command/handler.go
@@ -3,6 +3,7 @@ package command
 import (
 	"log"
 	"regexp"
+	"strconv"
 	"strings"
 
 	"github.com/miladbarzideh/goldis/internal/datastore"
@@ -12,6 +13,10 @@ const (
 	getCommand     = "get"
 	setCommand     = "set"
 	delCommand     = "del"
+	zaddCommand    = "zadd"
+	zremCommand    = "zrem"
+	zscoreCommand  = "zscore"
+	zqueryCommand  = "zquery"
 	syntaxErrorMsg = "(error) ERR syntax error"
 )
 
@@ -37,11 +42,36 @@ func (h *Handler) Execute(input string) string {
 		return h.dataSource.Get(args[0])
 	case strings.EqualFold(command, delCommand) && len(args) == 1:
 		return h.dataSource.Delete(args[0])
+	case strings.EqualFold(command, zaddCommand) && len(args) == 3:
+		score, err := strconv.ParseFloat(args[1], 64)
+		if err != nil {
+			return syntaxErrorMsg
+		}
+		return h.dataSource.ZAdd(args[0], score, args[2])
+	case strings.EqualFold(command, zremCommand) && len(args) == 2:
+		return h.dataSource.ZRemove(args[0], args[1])
+	case strings.EqualFold(command, zscoreCommand) && len(args) == 2:
+		return h.dataSource.ZScore(args[0], args[1])
+	case strings.EqualFold(command, zqueryCommand) && len(args) == 5:
+		score, err := strconv.ParseFloat(args[1], 64)
+		if err != nil {
+			return syntaxErrorMsg
+		}
+		offset, err := strconv.Atoi(args[3])
+		if err != nil {
+			return syntaxErrorMsg
+		}
+		limit, err := strconv.Atoi(args[4])
+		if err != nil {
+			return syntaxErrorMsg
+		}
+		return h.dataSource.ZQuery(args[0], score, args[2], uint32(offset), uint32(limit))
+
 	}
 	return syntaxErrorMsg
 }
 
 func extractCommandParts(input string) []string {
-	regex := regexp.MustCompile(`\w+`)
+	regex := regexp.MustCompile(`[^\s]+`)
 	return regex.FindAllString(input, -1)
 }
