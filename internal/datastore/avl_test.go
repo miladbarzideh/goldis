6900c812ff10869c3fcff9ef3f6cf323de12ea09
Milad Barzideh <milad.barzideh@snapp.cab>
2023-08-23 16:26:17 +0330

    test(avl): add more tests to validate the avl rebalance logic

--- internal/datastore/avl_test.go
+++ internal/datastore/avl_test.go
@@ -25,18 +25,6 @@ func TestAVLTree_Insert(t *testing.T) {
 	}
 }
 
-func TestAVLTree_Remove(t *testing.T) {
-	tree := NewAVLTree()
-	entry := NewZNode("milad", 20)
-	tree.Insert(&entry.tree, avlEntryEq)
-
-	tree.Remove(&entry.tree, avlEntryEq)
-
-	if tree.root != nil {
-		t.Errorf("Expected root to be nil, got %v", tree.root)
-	}
-}
-
 func TestAVLTree_Search(t *testing.T) {
 	tree := NewAVLTree()
 	entry1 := NewZNode("milad", 20)
@@ -50,6 +38,133 @@ func TestAVLTree_Search(t *testing.T) {
 	}
 	if foundNode != &entry2.tree {
 		t.Errorf("Expected found node to be %v, got %v", &entry2.tree, foundNode)
+	}
+}
+
+func TestAVLTree_RightRotate(t *testing.T) {
+	tree := NewAVLTree()
+	entry1 := NewZNode("milad", 3)
+	entry2 := NewZNode("ali", 2)
+	entry3 := NewZNode("ali", 1)
+	tree.Insert(&entry1.tree, avlEntryEq)
+	tree.Insert(&entry2.tree, avlEntryEq)
+	tree.Insert(&entry3.tree, avlEntryEq)
+
+	h := tree.root.getHeight()
+	if h != 1 {
+		t.Errorf("Expected height to be 1, got %v", h)
+	}
+	if tree.root.left == nil {
+		t.Errorf("Expected left child not to be null")
+	}
+	if tree.root.right == nil {
+		t.Errorf("Expected ritgh child not to be null")
+	}
+}
+
+func TestAVLTree_LeftRotate(t *testing.T) {
+	tree := NewAVLTree()
+	entry1 := NewZNode("milad", 1)
+	entry2 := NewZNode("ali", 2)
+	entry3 := NewZNode("ali", 3)
+	tree.Insert(&entry1.tree, avlEntryEq)
+	tree.Insert(&entry2.tree, avlEntryEq)
+	tree.Insert(&entry3.tree, avlEntryEq)
+
+	h := tree.root.getHeight()
+	if h != 1 {
+		t.Errorf("Expected height to be 1, got %v", h)
+	}
+	if tree.root.left == nil {
+		t.Errorf("Expected left child not to be null")
+	}
+	if tree.root.right == nil {
+		t.Errorf("Expected ritgh child not to be null")
+	}
+}
+
+func TestAVLTree_DoubleLeftRotate(t *testing.T) {
+	tree := NewAVLTree()
+	entry1 := NewZNode("milad", 7)
+	entry2 := NewZNode("ali", 9)
+	entry3 := NewZNode("ali", 6)
+	tree.Insert(&entry1.tree, avlEntryEq)
+	tree.Insert(&entry2.tree, avlEntryEq)
+	tree.Insert(&entry3.tree, avlEntryEq)
+
+	h := tree.root.getHeight()
+	if h != 1 {
+		t.Errorf("Expected height to be 1, got %v", h)
+	}
+	if tree.root.left == nil {
+		t.Errorf("Expected left child not to be null")
+	}
+	if tree.root.right == nil {
+		t.Errorf("Expected ritgh child not to be null")
+	}
+}
+
+func TestAVLTree_DoubleRightRotate(t *testing.T) {
+	tree := NewAVLTree()
+	entry1 := NewZNode("milad", 9)
+	entry2 := NewZNode("ali", 5)
+	entry3 := NewZNode("ali", 7)
+	tree.Insert(&entry1.tree, avlEntryEq)
+	tree.Insert(&entry2.tree, avlEntryEq)
+	tree.Insert(&entry3.tree, avlEntryEq)
+
+	h := tree.root.getHeight()
+	if h != 1 {
+		t.Errorf("Expected height to be 1, got %v", h)
+	}
+	if tree.root.left == nil {
+		t.Errorf("Expected left child not to be null")
+	}
+	if tree.root.right == nil {
+		t.Errorf("Expected ritgh child not to be null")
+	}
+}
+
+func TestAVLTree_RemoveRoot(t *testing.T) {
+	tree := NewAVLTree()
+	entry := NewZNode("milad", 20)
+	tree.Insert(&entry.tree, avlEntryEq)
+
+	tree.Remove(&entry.tree, avlEntryEq)
+
+	if tree.root != nil {
+		t.Errorf("Expected root to be nil, got %v", tree.root)
+	}
+}
+
+func TestAVLTree_RemoveOneChild(t *testing.T) {
+	tree := NewAVLTree()
+	entry1 := NewZNode("milad", 20)
+	entry2 := NewZNode("milad", 18)
+	tree.Insert(&entry1.tree, avlEntryEq)
+	tree.Insert(&entry2.tree, avlEntryEq)
+
+	tree.Remove(&entry1.tree, avlEntryEq)
+
+	if tree.root.getHeight() != 0 {
+		t.Errorf("Expected height to be 0, got %v", tree.root.getHeight())
+	}
+}
+
+func TestAVLTree_RemoveTwoChild(t *testing.T) {
+	tree := NewAVLTree()
+	entry1 := NewZNode("milad", 20)
+	entry2 := NewZNode("milad", 19)
+	entry3 := NewZNode("milad", 18)
+	tree.Insert(&entry1.tree, avlEntryEq)
+	tree.Insert(&entry2.tree, avlEntryEq)
+	tree.Insert(&entry3.tree, avlEntryEq)
+
+	tree.Remove(&entry2.tree, avlEntryEq)
+
+	foundNode := tree.Search(&entry2.tree, avlEntryEq)
+	if foundNode != nil {
+		t.Errorf("Expected foundNode to be nil, got %v", foundNode)
 	}
 }
 
