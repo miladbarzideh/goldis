9e2cb74541f4ee114760b5619bfa16c47be00b01
Milad Barzideh <milad.barzideh@snapp.cab>
2023-09-01 15:31:45 +0330

    test(heap): add a unit test to remove an item

--- internal/datastore/heap_test.go
+++ internal/datastore/heap_test.go
@@ -35,3 +35,24 @@ func TestMinHeap_Update(t *testing.T) {
 		}
 	}
 }
+
+func TestMinHeap_Remove(t *testing.T) {
+	h := NewMinHeap()
+	index1, index2, index3, index4 := int32(0), int32(0), int32(0), int32(0)
+	h.Insert(HeapItem{value: 5, ref: &index1})
+	h.Insert(HeapItem{value: 2, ref: &index2})
+	h.Insert(HeapItem{value: 9, ref: &index3})
+	h.Insert(HeapItem{value: 1, ref: &index4})
+
+	h.Remove(1)
+
+	expected := []int64{1, 5, 9}
+	for i, value := range expected {
+		if value != h.Get(int32(i)).value {
+			t.Errorf("Expected value %d at index %d, got %d", value, i, h.Get(int32(i)).value)
+		}
+		if int32(i) != *h.Get(int32(i)).ref {
+			t.Errorf("Expected index to be %d, got %d", i, *h.Get(int32(i)).ref)
+		}
+	}
+}
