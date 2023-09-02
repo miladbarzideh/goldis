138f6dcf50ca199e0a192a30bf90ffe98bae5482
Milad Barzideh <milad.barzideh@snapp.cab>
2023-09-02 13:16:02 +0330

    refactor(command): a concrete implementation for zadd command

---
+++ internal/command/actions/zadd.go
@@ -0,0 +1,26 @@
+package actions
+
+import (
+	"strconv"
+
+	"github.com/miladbarzideh/goldis/internal/datastore"
+)
+
+type ZAddCommand struct {
+	dataStore datastore.DataStore
+}
+
+func NewZAddCommand(dataStore datastore.DataStore) *ZAddCommand {
+	return &ZAddCommand{dataStore: dataStore}
+}
+
+func (c *ZAddCommand) Execute(args []string) string {
+	if len(args) == 3 {
+		score, err := strconv.ParseFloat(args[1], 64)
+		if err != nil {
+			return SyntaxErrorMsg
+		}
+		return c.dataStore.ZAdd(args[0], score, args[2])
+	}
+	return SyntaxErrorMsg
+}
