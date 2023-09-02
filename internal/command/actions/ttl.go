55324c0aa0d13de13723617cb82fe7fd15b25c27
Milad Barzideh <milad.barzideh@snapp.cab>
2023-09-02 13:15:56 +0330

    refactor(command): a concrete implementation for ttl command

---
+++ internal/command/actions/ttl.go
@@ -0,0 +1,20 @@
+package actions
+
+import (
+	"github.com/miladbarzideh/goldis/internal/datastore"
+)
+
+type TTLCommand struct {
+	dataStore datastore.DataStore
+}
+
+func NewTTLCommand(dataStore datastore.DataStore) *TTLCommand {
+	return &TTLCommand{dataStore: dataStore}
+}
+
+func (c *TTLCommand) Execute(args []string) string {
+	if len(args) == 1 {
+		return c.dataStore.Ttl(args[0])
+	}
+	return SyntaxErrorMsg
+}
