7e10118bbc04fc3bcc8e8848d34a1f73a2f52e36
Milad Barzideh <milad.barzideh@snapp.cab>
2023-09-02 13:15:25 +0330

    refactor(command): a concrete implementation for expire command

---
+++ internal/command/actions/expire.go
@@ -0,0 +1,26 @@
+package actions
+
+import (
+	"strconv"
+
+	"github.com/miladbarzideh/goldis/internal/datastore"
+)
+
+type ExpireCommand struct {
+	dataStore datastore.DataStore
+}
+
+func NewExpireCommand(dataStore datastore.DataStore) *ExpireCommand {
+	return &ExpireCommand{dataStore: dataStore}
+}
+
+func (c *ExpireCommand) Execute(args []string) string {
+	if len(args) == 2 {
+		ttl, err := strconv.Atoi(args[1])
+		if err != nil {
+			return SyntaxErrorMsg
+		}
+		return c.dataStore.Expire(args[0], int64(ttl))
+	}
+	return SyntaxErrorMsg
+}
