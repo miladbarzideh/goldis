4c6e791553142bc0b6f3eedde4e1bca7b7301f8f
Milad Barzideh <milad.barzideh@snapp.cab>
2023-09-02 13:15:30 +0330

    refactor(command): a concrete implementation for get command

---
+++ internal/command/actions/get.go
@@ -0,0 +1,20 @@
+package actions
+
+import (
+	"github.com/miladbarzideh/goldis/internal/datastore"
+)
+
+type GetCommand struct {
+	dataStore datastore.DataStore
+}
+
+func NewGetCommand(dataStore datastore.DataStore) *GetCommand {
+	return &GetCommand{dataStore: dataStore}
+}
+
+func (c *GetCommand) Execute(args []string) string {
+	if len(args) == 1 {
+		return c.dataStore.Get(args[0])
+	}
+	return SyntaxErrorMsg
+}
