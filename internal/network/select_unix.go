e717fee09e446c06b2798a6b42ceceb6e88bb32d
Milad Barzideh <milad.barzideh@snapp.cab>
2023-09-20 00:12:12 +0330

    refactor(network): there's no need to re-declare err

--- internal/network/select_unix.go
+++ internal/network/select_unix.go
@@ -5,6 +5,6 @@ package network
 import "syscall"
 
 func platformSpecificSelect(n int, r *syscall.FdSet, w *syscall.FdSet, e *syscall.FdSet, timeout *syscall.Timeval) (err error) {
-	_, err := syscall.Select(n, r, w, e, timeout)
+	_, err = syscall.Select(n, r, w, e, timeout)
 	return err
 }
